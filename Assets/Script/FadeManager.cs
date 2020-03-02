7f1597b6abafdb97ce5645581eba54169adac4b7
noiva-UN <opjwb1@gmail.com>
2020-03-02 17:37:22 +0900

    タイトルシーン作成

---
+++ Assets/Script/FadeManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+    private static Canvas fadeCanvas;
+    private static Image fadeImage;
+
+    private static float alpha = 0.0f;
+
+    public static bool isFadeIn = false;
+    public static bool isFadeOut = false;
+
+    private static float fadeTime = 0.2f;
+
+    private static int nextScene = 1;
+
+
+    static void Init()
+    {
+        var fadeObject = new GameObject("CanvasFade");
+        fadeCanvas = fadeObject.AddComponent<Canvas>();
+        fadeObject.AddComponent<GraphicRaycaster>();
+        fadeCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        fadeObject.AddComponent<FadeManager>();
+        DontDestroyOnLoad(fadeObject);
+        
+        fadeCanvas.sortingOrder = 100;
+
+        fadeImage = new GameObject("ImageFade").AddComponent<Image>();
+        fadeImage.transform.SetParent(fadeCanvas.transform, false);
+        fadeImage.rectTransform.anchoredPosition = Vector3.zero;
+        DontDestroyOnLoad(fadeImage);
+        
+        fadeImage.rectTransform.sizeDelta = new Vector2(9999, 9999);
+    }
+
+    public static void FadeIn()
+    {
+        if (fadeImage == null) Init();
+        fadeImage.color=Color.clear;
+        isFadeIn = true;
+    }
+
+    public static void FadeOut(int n)
+    {
+        if(fadeImage==null) Init();
+        nextScene = n;
+        fadeImage.color = Color.clear;
+        isFadeOut = true;
+    }
+
+    void Update()
+    {
+        if (isFadeIn)
+        {
+            alpha -= Time.deltaTime / fadeTime;
+
+            if (alpha <= 0.0f)
+            {
+                isFadeIn = false;
+                alpha = 0.0f;
+                fadeCanvas.enabled = false;
+            }
+
+            fadeImage.color = new Color(0.0f, 0.0f, 0.0f, alpha);
+        }else if (isFadeOut)
+        {
+            alpha += Time.deltaTime / fadeTime;
+
+            if (alpha >= 1.0f)
+            {
+                isFadeOut = false;
+                alpha = 1.0f;
+
+                SceneManager.LoadScene(nextScene);
+            }
+            
+            fadeImage.color=new Color(0.0f,0.0f,0.0f,alpha);
+        }
+    }
+}
---
+++ Assets/Script/StartScenesManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScenesManager : MonoBehaviour
+{
+    [SerializeField] private Text message,title;
+
+    [SerializeField] private GameObject buttons;
+
+    [SerializeField] private float titleMoveSpeed = 1f;
+    
+    private int phase = 0;
+    
+    void Awake()
+    {
+        FadeManager.FadeIn();
+        Init();
+        
+    }
+
+    void Update()
+    {
+        if (Input.anyKey && phase==0)
+        {
+            phase++;
+            StartCoroutine(MoveTitle());
+        }
+    }
+
+    private void Init()
+    {
+        buttons.SetActive(false);
+        title.rectTransform.anchoredPosition=Vector3.zero;
+        message.text = "Push any key or button";
+        phase = 0;
+    }
+
+    IEnumerator MoveTitle()
+    {
+        while (true)
+        {
+            var now = title.rectTransform.anchoredPosition;
+
+            now.y += titleMoveSpeed * Time.deltaTime;
+            title.rectTransform.anchoredPosition = now;
+
+            if (now.y >= 120)
+            {
+                title.rectTransform.anchoredPosition = new Vector3(0f, 120f, 0f);
+                buttons.SetActive(true);
+                break;
+            }
+
+            yield return null;
+        }
+    }
+
+    public void ButtonAct(int n)
+    {
+        FadeManager.FadeOut(n);
+    }
+}
